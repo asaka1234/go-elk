1b85da4fae487c9b686fd7d7a87ed9a10cc5423e
cyrildou <cyrildou@live.cn>
2025-05-20 16:38:13 +0800

    add verify

--- req_cry_withdraw_callback.go
+++ req_cry_withdraw_callback.go
@@ -1,9 +1,36 @@
 package go_elk
 
+import (
+	"encoding/json"
+	"github.com/asaka1234/go-elk/utils"
+	"log"
+)
+
 // 充值/提现的回调处理(传入一个处理函数)
 func (cli *Client) CryWithdrawCallback(req ELKCryWithdrawBackReq, processor func(ELKCryWithdrawBackReq) error) error {
 	//验证签名
-	//TODO
+	//验证签名
+	params := make(map[string]interface{})
+	jsonData, err := json.Marshal(req)
+	if err != nil {
+		log.Printf("JSON marshal error: %v", err)
+		return err
+	}
+	if err := json.Unmarshal(jsonData, &params); err != nil {
+		log.Printf("JSON unmarshal error: %v", err)
+		return err
+	}
+
+	// Verify signature
+	flag, err := utils.Verify(params, cli.BackKey)
+	if err != nil {
+		log.Printf("Signature verification error: %v", err)
+		return err
+	}
+	if !flag {
+		reqJson, _ := json.Marshal(req)
+		log.Printf("ELKCur back verify fail, req: %s", string(reqJson))
+	}
 
 	//开始处理
 	return processor(req)
