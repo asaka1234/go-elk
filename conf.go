81948646d013826901652d520dcd10528e01c60e
cyrildou <cyrildou@live.cn>
2025-05-23 01:01:16 +0800

    up

---
+++ conf.go
@@ -0,0 +1,16 @@
+package go_elk
+
+const (
+	//参数
+	MERCHANT_ID = 52276
+	ACCESS_KEY  = "wkHtvU1nvpEC6"
+	BACK_KEY    = "qIs3uWYbznn"
+
+	//法币
+	CUR_DEPOSIT_URL  = "https://127.0.0.1/coin/pay/order/pay/checkout/counter"
+	CUR_WITHDRAW_URL = "https://127.0.0.1/coin/pay/order/pay/withdrawal"
+
+	//crypto
+	CRY_DEPOSIT_URL  = "https://127.0.0.1/coin/pay/order/coin/checkout/counter"
+	CRY_WITHDRAW_URL = "https://127.0.0.1/coin/pay/merchant/create/withdraw"
+)
